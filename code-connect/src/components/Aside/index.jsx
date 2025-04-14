a7fc810550858a5d17ed7a78946c3057d7e184f5
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-14 20:12:11 -0300

    feat: Criando o componente Aside

---
+++ code-connect/src/components/Aside/index.jsx
@@ -0,0 +1,9 @@
+import S from "./asside.module.css";
+
+export const Aside = () => {
+  return (
+    <aside className={S.aside}>
+      <img src="/logo.png" alt="Logo da Code Connect" />
+    </aside>
+  );
+};
