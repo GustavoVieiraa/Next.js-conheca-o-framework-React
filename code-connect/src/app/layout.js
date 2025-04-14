604d78b3a2f5c00b64a9d600420e1a469adc136a
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-14 20:21:33 -0300

    feat: Implementado o Componente Image do Next.js

--- code-connect/src/app/layout.js
+++ code-connect/src/app/layout.js
@@ -9,8 +9,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body>
-        <Aside />
-        {children}
+        <div className='app-container'>
+          <Aside />
+          {children}
+        </div>
       </body>
     </html>
   );
--- code-connect/src/components/Aside/index.jsx
+++ code-connect/src/components/Aside/index.jsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import S from "./asside.module.css";
+
+import logo from "./logo.png";
 
 export const Aside = () => {
   return (
     <aside className={S.aside}>
-      <img src="/logo.png" alt="Logo da Code Connect" />
+      {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
+      <Image src={logo} alt="Logo da Code Connect" />
     </aside>
   );
 };
