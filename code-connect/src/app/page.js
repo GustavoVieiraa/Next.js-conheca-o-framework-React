2dd364cb32e798db1a01afdeed4583a52d8eceb5
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-14 23:15:16 -0300

    modified:   code-connect/combined.log modified:   code-connect/src/app/page.js modified:   code-connect/combined.log

--- code-connect/src/app/page.js
+++ code-connect/src/app/page.js
@@ -3,6 +3,7 @@ import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
 
 import S from "./page.module.css";
+import Link from "next/link";
 
 async function getAllPosts (page) {
   const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
@@ -18,11 +19,13 @@ async function getAllPosts (page) {
 
 export default async function Home() {
 
-  const { data: posts } = await getAllPosts(1);
+  const { data: posts, prev, next } = await getAllPosts(1);
 
   return (
     <main className={S.grid}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
+      {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+      {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
     </main>
   );
 }
